ce24c681d136d57bb801cca92fd1620d961abd9e
Jhey <macarubbo.david@gmail.com>
2018-02-17 18:58:52 +0800

    Added new vue instance, fixed healthbar size by HP.

---
+++ app.js
@@ -0,0 +1,14 @@
+new Vue({
+    el: "#app",
+    data: {
+        playerHp: 100,
+        enemyHp: 100,
+        gameIsRunning: false
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+});
