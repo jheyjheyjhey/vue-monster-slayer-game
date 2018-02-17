2edcf5388a17b09df7571b55fb08eda891a784f3
Jhey <macarubbo.david@gmail.com>
2018-02-17 19:18:35 +0800

    Added regular attack functionality.

--- app.js
+++ app.js
@@ -8,10 +8,45 @@ new Vue({
     methods: {
         startGame: function () {
             this.gameIsRunning = true;
-            
             // Ensure the game resets on a new game
             this.playerHp = 100;
             this.enemyHp = 100;
+        },
+        // Regular Attack
+        attack: function () {
+            var maxDmg = 10;
+            var minDmg = 3;
+            // Set the damage randomly (Max damage is 10, min damage is 3)
+            var dmg = Math.max(Math.floor(Math.random() * maxDmg) + 1, minDmg);
+            this.enemyHp -= dmg;
+
+            if (this.enemyHp <= 0) {
+                alert('Winner winner chicken dinner!');
+                this.gameIsRunning = false;
+                return;
+            }
+
+            max = 12;
+            min = 5;
+            dmg = Math.max(Math.floor(Math.random() * maxDmg) + 1, minDmg);
+            this.playerHp -= dmg;
+
+            if (this.playerHp <= 0) {
+                alert('You lost! :(');
+                this.gameIsRunning = false;
+                return;
+            }
+
+
+        },
+        specialAtk: function () {
+
+        },
+        heal: function () {
+
+        },
+        giveUp: function () {
+
         }
     },
     computed: {
